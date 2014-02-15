e916270b32111438e25d4a23db5e7ab9c0fae977
ericpierce <eric.d.pierce@gmail.com>
2014-02-15 10:53:55 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+gTunes
+
+Just going through the example app in The Definitive Guide to Grails 2 by Jeff Scott Brown and Graeme Rocher
---
+++ grails-app/controllers/gtunes/StoreController.groovy
@@ -0,0 +1,9 @@
+package gtunes
+
+class StoreController {
+
+    def index() {
+        render 'Welcome to the gTunes store!'
+    }
+
+}
