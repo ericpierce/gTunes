09bea2ee3aa1dbf2b39a383c7d143912b3674e4a
ericpierce <eric.d.pierce@gmail.com>
2014-02-18 14:20:25 -0600

    pg 35

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -21,8 +21,12 @@ environments {
     }
     test {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            dialect = "org.hibernate.dialect.SQLServerDialect"
+            dbCreate = ""
+            driverClassName = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+            username = ""
+            password = ""
+            url = "jdbc:sqlserver://localhost:1433;databaseName=1stcomp"
         }
     }
     production {
